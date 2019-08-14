1db063cddf2b4355b0cb715c173aea6126742364
yd.yu <dongyang.yu@anxincloud.com>
2019-08-14 20:03:36 +0800

    init commit

---
+++ src/main/java/com/doubleysoft/alg/leetcode/array/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+package com.doubleysoft.alg.leetcode.array;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ */
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        int start = 0, end = len - 1;
+        while (start <= end) {
+            int middle = (end - start) / 2 + start;
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[middle] < target) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/test/java/com/doubleysoft/alg/leetcode/array/SearchInsertPositionTest.java
@@ -0,0 +1,69 @@
+package com.doubleysoft.alg.leetcode.array;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dongyang.yu
+ * @email dongyang.yu@anxincloud.com
+ */
+public class SearchInsertPositionTest {
+    private static List<SearchInsertPositionTestData> testData;
+
+    static {
+        testData = new ArrayList<SearchInsertPositionTestData>() {{
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1, 3, 5, 6})
+                    .target(5)
+                    .answer(2)
+                    .build());
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1, 3, 5, 6})
+                    .target(2)
+                    .answer(1)
+                    .build());
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1, 3, 5, 6})
+                    .target(7)
+                    .answer(4)
+                    .build());
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1, 3, 5, 6})
+                    .target(0)
+                    .answer(0)
+                    .build());
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1})
+                    .target(0)
+                    .answer(0)
+                    .build());
+            add(SearchInsertPositionTestData.builder()
+                    .arr(new int[]{1})
+                    .target(2)
+                    .answer(1)
+                    .build());
+        }};
+    }
+
+    @Test
+    public void searchInsert() {
+        testData.forEach(row -> {
+            Assert.assertEquals(SearchInsertPosition.searchInsert(row.getArr(), row.getTarget()), row.getAnswer());
+        });
+    }
+
+    @Getter
+    @Builder
+    static class SearchInsertPositionTestData {
+        private int[] arr;
+
+        private int target;
+
+        private int answer;
+    }
+}
