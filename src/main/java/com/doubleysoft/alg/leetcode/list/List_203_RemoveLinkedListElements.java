8ac4051cbcf2aef559b0d9af9146a894e8d5d3bd
yd.yu <dongyang.yu@anxincloud.com>
2019-11-20 21:03:04 +0800

    add list 203

---
+++ src/main/java/com/doubleysoft/alg/leetcode/list/List_203_RemoveLinkedListElements.java
@@ -0,0 +1,39 @@
+package com.doubleysoft.alg.leetcode.list;
+
+/**
+ * 203. Remove Linked List Elements
+ * Remove all elements from a linked list of integers that have value val.
+ * <p>
+ * Example:
+ * <p>
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ *
+ * @see <a href="https://leetcode.com/problems/remove-linked-list-elements/">203. Remove Linked List Elements</a>
+ */
+public class List_203_RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode tmp = head;
+        ListNode newHead = head;
+        if (tmp.val == val) {
+            while (tmp != null && tmp.val == val) {
+                tmp = tmp.next;
+            }
+            newHead = tmp;
+        }
+        if (newHead == null) {
+            return newHead;
+        }
+        tmp = newHead;
+        while (tmp != null) {
+            while ((tmp.next != null && tmp.next.val == val)) {
+                tmp.next = tmp.next.next;
+            }
+            tmp = tmp.next;
+        }
+        return newHead;
+    }
+}
---
+++ src/test/java/com/doubleysoft/alg/leetcode/list/List_203_RemoveLinkedListElementsTest.java
@@ -0,0 +1,41 @@
+package com.doubleysoft.alg.leetcode.list;
+
+import com.doubleysoft.alg.leetcode.CommonUtil;
+import com.doubleysoft.alg.leetcode.Tuple3;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author dongyang.yu
+ * @email dongyang.yu@anxincloud.com
+ */
+public class List_203_RemoveLinkedListElementsTest {
+    private static List<Tuple3<ListNode, Integer, ListNode>> testData;
+    private List_203_RemoveLinkedListElements list_203_removeLinkedListElements;
+
+
+    @Before
+    public void setUp() throws Exception {
+        list_203_removeLinkedListElements = new List_203_RemoveLinkedListElements();
+        testData = Arrays.asList(
+                new Tuple3<>(ListNode.build(), 1, ListNode.build()),
+                new Tuple3<>(ListNode.build(1, 2, 3, 5), 3, ListNode.build(1, 2, 5)),
+                new Tuple3<>(ListNode.build(2, 2, 3, 2), 2, ListNode.build(3)),
+                new Tuple3<>(ListNode.build(1, 2, 2, 1), 2, ListNode.build(1, 1))
+
+        );
+    }
+
+    @Test
+    public void removeElements() {
+        testData.forEach(row -> {
+            ListNode listNode = list_203_removeLinkedListElements.removeElements(row.getFirst(), row.getSecound());
+            ListNodeTestUtil.assertEqual(listNode, row.getThird());
+        });
+    }
+}
