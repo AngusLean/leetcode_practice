5d8109bb891ab434bfa5d0bedb27f7dcd574bca7
anguslean <cupofish@gmail.com>
2019-12-25 23:47:48 +0800

    add atoi imp

---
+++ src/main/java/com/doubleysoft/alg/leetcode/strings/String_8StringtoInteger.java
@@ -0,0 +1,35 @@
+package com.doubleysoft.alg.leetcode.strings;
+
+public class String_8StringtoInteger {
+    public int myAtoi(String str) {
+        if (str == null || str.trim().length() == 0) {
+            return 0;
+        }
+        int len = str.length();
+        boolean negative = false;
+        int i = 0;
+        //1. skip all whitespace
+        while (str.charAt(i) == ' ') {
+            i++;
+        }
+        //2. determine sign
+        if (str.charAt(i) == '-') {
+            negative = true;
+            i++;
+        } else if (str.charAt(i) == '+') {
+            negative = false;
+            i++;
+        }
+
+        int result = 0;
+        while (i < len && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            //fuck check. because inter max value is 2,147,483,647
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10) && (str.charAt(i) - '0') > 7) {
+                return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            result = result * 10 + (str.charAt(i++) - '0');
+        }
+        return negative ? result * -1 : result;
+    }
+
+}
---
+++ src/test/java/com/doubleysoft/alg/leetcode/strings/String_8StringtoIntegerTest.java
@@ -0,0 +1,35 @@
+package com.doubleysoft.alg.leetcode.strings;
+
+import com.doubleysoft.alg.leetcode.Tuple;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class String_8StringtoIntegerTest {
+    private static List<Tuple<String, Integer>> testData;
+    private String_8StringtoInteger string_8StringtoInteger;
+
+    @Before
+    public void setUp() throws Exception {
+        string_8StringtoInteger = new String_8StringtoInteger();
+        testData = Arrays.asList(
+                new Tuple<>("  123", 123),
+                new Tuple<>("  123abc", 123),
+                new Tuple<>("  -123abc", -123),
+                new Tuple<>("  wdsfsdf", 0),
+                new Tuple<>("-91283472332", Integer.MIN_VALUE),
+                new Tuple<>("2147483648", 2147483647)
+        );
+    }
+
+    @Test
+    public void myAtoi() {
+        testData.forEach(row -> {
+            int result = string_8StringtoInteger.myAtoi(row.getFirst());
+            Assert.assertTrue(result == row.getSecound());
+        });
+    }
+}
