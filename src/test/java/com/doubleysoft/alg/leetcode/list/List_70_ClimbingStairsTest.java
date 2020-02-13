637ef397f3611115236778886cdf5026a1db4971
anguslean <cupofish@gmail.com>
2020-02-13 18:34:35 +0800

    add q 70

---
+++ src/test/java/com/doubleysoft/alg/leetcode/list/List_70_ClimbingStairsTest.java
@@ -0,0 +1,35 @@
+package com.doubleysoft.alg.leetcode.list;
+
+import com.doubleysoft.alg.leetcode.Tuple;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_70_ClimbingStairsTest {
+    private List<Tuple<Integer, Integer>> testData;
+
+    private List_70_ClimbingStairs list_70_climbingStairs;
+
+
+    @Before
+    public void setUp() throws Exception {
+        list_70_climbingStairs = new List_70_ClimbingStairs();
+        testData = new ArrayList<Tuple<Integer, Integer>>() {{
+            add(new Tuple<>(2, 2));
+            add(new Tuple<>(3, 3));
+            add(new Tuple<>(1, 1));
+            add(new Tuple<>(45, 1));
+        }};
+    }
+
+    @Test
+    public void climbStairs() {
+        testData.forEach(row -> {
+            System.out.println("test row: " + row);
+            Assert.assertTrue(row.getSecound() == list_70_climbingStairs.climbStairs(row.getFirst()));
+        });
+    }
+}
